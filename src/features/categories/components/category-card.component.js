d2dd1f63f65bcaab46ecfe8169aca5b1ae56d456
skywalker-alt327 <itisgoingtobeavictory2.39@gmail.com>
2022-06-22 14:51:46 +0600

    added category card and cleaned up a bit

--- src/features/categories/components/category-card.component.js
+++ src/features/categories/components/category-card.component.js
@@ -1,22 +1,18 @@
 import React from "react";
-import { Image, TouchableOpacity } from "react-native";
+import { TouchableOpacity } from "react-native";
 import {
   CategoryItemContainer,
   CategoryItemImage,
   CategoryItemName,
   CategoryItemInfo,
 } from "./category-card.styles";
-// import { styles } from "./category-card.styles";
 import { getNumberOfRecipes } from "../../../services/data/mock.data.api";
 
 export const CategoryItemCard = ({ item }) => {
   return (
     <TouchableOpacity onPress={() => onPressCategory(item)}>
       <CategoryItemContainer>
-        <CategoryItemImage
-          // style={styles.categoriesPhoto}
-          source={{ uri: item.photo_url }}
-        />
+        <CategoryItemImage source={{ uri: item.photo_url }} />
         <CategoryItemName>{item.name}</CategoryItemName>
         <CategoryItemInfo>
           {getNumberOfRecipes(item.id)} recipes
